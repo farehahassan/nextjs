0583259fa81f55e4b6f89456db71f3f6d2f4be15
Fareha <hfariha104@gmail.com>
2023-10-20 23:08:18 +0500

    updated pages

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+// import React from 'react'
+import Herosection from '../components/Herosection';
+const About = () => {
+  return (
+    
+      <Herosection/>
+    
+  );
+};
+
+export default About;
